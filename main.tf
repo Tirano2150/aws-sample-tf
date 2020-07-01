b5d563d29fd3fd68c906f952619ff54c3d2e508b
Tim Ellis <tirano2150@hotmail.com>
2020-07-01 15:50:39 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_instance" "example" {
   ami           = "ami-005af4c3162f495fa"
