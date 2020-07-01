f9492337c9d9539875ca93d9ba41e9569f7c4fb5
Tim Ellis <tirano2150@hotmail.com>
2020-07-01 16:23:29 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
   profile = "default"
   region  = "eu-west-1"
 }
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-state-eu-west-1-122072647213"
+    key    = "sample/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
